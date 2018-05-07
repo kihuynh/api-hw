f7906f25281f1ec27211f90e359dff8e62e747f7
Student <example@example.com>
2018-05-07 11:54:18 -0700

    spec for post routes

--- spec/requests/get_quotes_spec.rb
+++ spec/requests/get_quotes_spec.rb
@@ -5,10 +5,11 @@ describe "get all quotes route", :type => :request do
 
   before { get '/quotes'}
 
+# first test
   it 'returns all quotes' do
     expect(JSON.parse(response.body).size).to eq(20)
   end
-
+# second test
   it 'returns status code 200' do
     expect(response).to have_http_status(:success)
   end
